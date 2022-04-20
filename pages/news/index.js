f80bd122cd6cd3db13bc557393bb41e0d7a03b41
nenadvasilic <vasilic2013@gmail.com>
2022-04-20 12:33:27 +0200

    commit

---
+++ pages/news/index.js
@@ -0,0 +1,23 @@
+// our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJs Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
